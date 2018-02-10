1212529459efbe254be30be858c009b510785d0f
paulamp <paulapliegom@gmail.com>
2018-02-10 18:29:42 +0100

    entrega

---
+++ servidor-random-http.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""
+Simple HTTP Server version 2: reuses the port, so it can be
+restarted right after it has been killed. Accepts connects from
+the outside world, by binding to the primary interface of the host.
+
+Jesus M. Gonzalez-Barahona and Gregorio Robles
+{jgb, grex} @ gsyc.es
+SAT and SARO subjects (Universidad Rey Juan Carlos)
+"""
+
+import socket
+import random
+
+# Create a TCP objet socket and bind it to a port
+# Port should be 80, but since it needs root privileges,
+# let's use one above 1024
+
+mySocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+mySocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+mySocket.bind(('localhost', 1234))
+
+# Queue a maximum of 5 TCP connection requests
+mySocket.listen(5)
+
+# Accept connections, read incoming data, and answer back an HTML page
+#  (in an almost-infinite loop; the loop can be stopped with Ctrl+C)
+
+try:
+    while True:
+        print('Waiting for connections')
+        (recvSocket, address) = mySocket.accept()
+        print('Request received:')
+        print(recvSocket.recv(2048))
+        print('Answering back...')
+        recvSocket.send(b"HTTP/1.1 200 OK\r\n\r\n" +
+                        b"<html><body><h1>Hola <a href='http://localhost:1234/" + str(random.randint(10,400000)) + "'>Dame otra direccion</a></h1>" +
+                        b"</body></html>" +
+                        b"\r\n")
+        recvSocket.close()
+except KeyboardInterrupt:
+    print("Closing binded socket")
+    mySocket.close()
